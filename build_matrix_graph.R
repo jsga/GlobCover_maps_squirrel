cd1007a0dfe4723eed9ac7e91a3476262993e310
jsga <javiersaezgallego@gmail.com>
2017-11-11 08:01:43 +0100

    Cleaned code + new dashboard

--- build_matrix_graph.R
+++ build_matrix_graph.R
@@ -43,43 +43,8 @@ build_matrix_graph = function(Ni,Nj,val_raster){
   return(mat_path)
 }
 
-build_matrix_graph_V2 = function(Ni,Nj){
-  
-  N = Ni*Nj
-  mat_path = Matrix(0,ncol=N,nrow=N,sparse = T)
-  
-  # lower blocl
-  ii = 1:(N-1)+1
-  jj = 1:(N-1)
-  mat_path[cbind(ii,jj)] = 1
-  
-  # uppwe block
-  ii = 1:(N-1)
-  jj = 1:(N-1)+1
-  mat_path[cbind(ii,jj)] = 1
-  
-  # make the (NiNj) zero
-  # [5,6] & [6,5] == 0
-  ii = c( Ni * 1:Ni, Ni * 1:Ni + 1)
-  jj = c( Nj * 1:Nj+1, Ni * 1:Nj)
-  idx = cbind(ii,jj)
-  idx_keep = apply(idx < N,1,all)
-  mat_path[idx[idx_keep,]] = 0
-  
-  # Make the upper long line
-  ii = 1:(N-Nj)
-  jj = Ni + 1:(N-Nj)
-  mat_path[cbind(ii,jj)] = 1
-  
-  # lower long line
-  ii = Nj + 1:(N-Ni)
-  jj = 1:(N-Ni)
-  mat_path[cbind(ii,jj)] = 1
-  
-  # END. Return
-  return(mat_path)
-}
 
+# Original version. Too long to compute.
 build_matrix_graph_V1 = function(Ni,Nj){
   # Initialize as a sparse matrix
   mat_path = Matrix(0,ncol=N,nrow=N,sparse = T)
